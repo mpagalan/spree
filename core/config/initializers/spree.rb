6814691f49a86e117dba68ac12a27cf770c945cf
Sean Schofield <sean@railsdog.com>
2010-06-24 15:59:05 -0400

    Removed the attribute_fu plugin.  This likely breaks some admin forms related to products, option_types and properties.  We'll fix those later (right now we need the plugins out of there for other reasons.)

--- core/config/initializers/spree.rb
+++ core/config/initializers/spree.rb
@@ -4,21 +4,11 @@ SESSION_KEY = '_spree_session_id'
 
 # TODO - Add the lib/plugins stuff maybe?
 
-# Initialize preference system (and attribute_fu)
-#
-# We need to hand-load attribute_fu stuff here because we call
-# ActiveRecord#has_many method before the attribute_fu plugin is
-# completely loaded.
+# Initialize preference system
 ActiveRecord::Base.class_eval do
-  include AttributeFu::Associations
   include Spree::Preferences
   include Spree::Preferences::ModelHooks
 end
-
-ActionView::Helpers::FormBuilder.class_eval do
-  include AttributeFu::AssociatedFormHelper
-end
-
 
 # Initialize mail server settings
 Spree::Preferences::MailSettings.init
@@ -32,13 +22,13 @@ Spree::Preferences::MailSettings.init
 # class ActiveRecord::Base
 #   # Ryan Bates - http://railscasts.com/episodes/112
 #   scope :conditions, lambda { |*args| where(args)}
-# 
+#
 #   # general merging of conditions, names following the searchlogic pattern
 #   # conditions_all is a more descriptively named enhancement of the above
 #   scope :conditions_all, lambda { |*args| where([args].flatten)}
-# 
+#
 #   # forming the disjunction of a list of conditions (as strings)
-#   scope :conditions_any, lambda { |*args| 
+#   scope :conditions_any, lambda { |*args|
 #     args = [args].flatten
 #     raise "non-strings in conditions_any" unless args.all? {|s| s.is_a? String}
 #     { where(args.map {|c| "(#{c})"}.join(" OR ")) }
